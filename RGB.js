d8d96915769fe2b2aa82aebc198331b46149880d
airborne17th <43795410+airborne17th@users.noreply.github.com>
2020-02-20 08:37:12 -0600

    Add files via upload

--- RGB.js
+++ RGB.js
@@ -1,26 +1,43 @@
 $(document).ready(function(){
 	// An event based animation with mouseover, hover, and mouseout. 
+	var colorArray = [red, blue, green];
+	var red = "red";
+	var blue = "blue";
+	var green = "green";
+	var colorChoice = "";
 	$("#gamer").mouseover(function(){
-		$(this).animate("color", "red");
+		colorChoice = Math.floor(Math.random() * (2));
+		colorChoice = colorArray[colorChoice];
+		$(this).animate({color: colorChoice}, 200);
+		console.log(colorChoice);
 	});
 	$("#gamer").mouseout(function(){
-		$(this).animate("color", "blue");
+		colorChoice = Math.floor(Math.random() * (2));
+		colorChoice = colorArray[colorChoice];
+		$(this).animate({color: colorChoice}, 200);
+		console.log(colorChoice);
 	});
 	$("#gamer").hover(function(){
-		$(this).each(function(i) {
-			$(this).delay(200).animate("color", "green");
-			$(this).delay(400).animate("color", "blue");
-			$(this).delay(600).animate("color", "red");
-		 });
-
+		colorChoice = Math.floor(Math.random() * (2));
+		colorChoice = colorArray[colorChoice];
+		$(this).animate({color: colorChoice}, 200);
+		console.log(colorChoice);
 	});
   });
 
 	/*
+	var colorArray = [red, blue, green];
 	var red = $("#gamer").css("color", "red");
 	var blue = $("#gamer").css("color", "blue");
 	var green = $("#gamer").css("color", "green");
-	var colorArray = [red, blue, green];
 	colorChoice = Math.floor(Math.random() * (2));
-	$("#gamer").css(colorArray[colorChoice]);
+	$(this).animate({color: colorChoice}, 200);
+	*/
+
+	/*
+	$(this).each(function(i) {
+			$(this).delay(200).animate("color", "green");
+			$(this).delay(400).animate("color", "blue");
+			$(this).delay(600).animate("color", "red");
+		 });
 	*/
--- expand.js
+++ expand.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 		$("#pic").mouseover(function(){
-			$(this).animate({ margin: 0, width: "-=25%", height: "-=25%" }, 800);
+			$(this).animate({ margin: 0, width: "+=25%", height: "+=25%" }, 800);
 			});
 		$("#pic").mouseout(function() {
 				$(this).animate({ margin: 0, width: "-=25%", height: "-=25%" }, 800);
